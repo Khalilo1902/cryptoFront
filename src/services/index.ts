084443be4ddd445f6c3dbbf92ffae299b452bc3d
khalil <khalil.haouas@gmail.com>
2024-10-06 23:04:21 +0200

    finish

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 
 
 export const SERVER_URL = "http://crypto-db.khalil-dev.me/api/v1";
+//export const SERVER_URL = "http://localhost:3005/api/v1";
+
 
 export const axiosJWT = axios.create();
 axiosJWT.interceptors.request.use;
