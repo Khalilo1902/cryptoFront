348bb6f80f0a4316068c9771e7ddbd84bd78e5bf
khalil <khalil.haouas@gmail.com>
2024-10-06 22:44:56 +0200

    finish

--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const SERVER_URL = "http://crypto-db.khalil-dev.me/api/v1";
-
-export const axiosJWT = axios.create();
-axiosJWT.interceptors.request.use;
--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { TUser } from "../interface";
 
 
-const SERVER_URL = "http://crypto-db.khalil-dev.me/api/v1";
+export const SERVER_URL = "http://crypto-db.khalil-dev.me/api/v1";
 
 export const axiosJWT = axios.create();
 axiosJWT.interceptors.request.use;
